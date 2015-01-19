4ae7d9a8307be3270d1ac71a0030a77f8905d003
Neil Ellis <neilellis@cazcade.com>
2015-01-19 13:49:35 +0000

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var server = vertx.createHttpServer();
 var routeMatcher = new vertx.RouteMatcher();
 
 routeMatcher.get('/date', function(req) {
-    req.response().end(new Date());
+    req.response().end(""+new Date());
 });
 
 server.requestHandler(routeMatcher).listen(8080);
