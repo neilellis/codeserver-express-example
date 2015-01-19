d4218c12138e838b498a8b8ca7d7eadeb8f23245
Neil Ellis <neilellis@cazcade.com>
2015-01-19 19:14:51 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var vertx = require('vertx');
 // Inspired from Sinatra / Express
 var rm = new vertx.RouteMatcher();
 
-// Extract the params from the uri
 rm.get('/details/:user/:id', function(req) {
   req.response.end("User: " + req.params().get('user') + " ID: " + req.params().get('id'))
 });
